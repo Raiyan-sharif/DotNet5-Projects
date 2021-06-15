df490dc25e9cdc56a8dd5506c406d3eb1a88225b
Raiayn Sharif <raiyan.sharif@aci-bd.com>
2021-06-16 01:10:38 +0600

    delete functionality for expenses added

--- InAndOut/InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/InAndOut/Controllers/ExpenseController.cs
@@ -26,6 +26,8 @@ namespace InAndOut.Controllers
             return View();
         }
 
+
+        //POST - Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Expense obj)
@@ -40,5 +42,41 @@ namespace InAndOut.Controllers
             return View(obj);
             
         }
+
+
+        //GET Delete
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            
+            var obj = _db.Expenses.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+
     }
 }
